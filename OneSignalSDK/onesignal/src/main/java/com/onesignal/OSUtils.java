992f2135ee179afec76c6b3d8ef579ce11892d0d
peterhu <wycgpeterhu@sina.com>
2021-07-02 15:42:50 +0800

    减少方法调用次数，减少anr

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OSUtils.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OSUtils.java
@@ -452,15 +452,36 @@ class OSUtils {
       return !TextUtils.isEmpty(body);
    }
 
+
+
+
+
+   static int areNoticificationsEnableCount = 0;
+   static boolean enableFlag = true;
+   static boolean noticificationState = true;
+
+
+
    // Get the app's permission which will be false if the user disabled notifications for the app
    //   from Settings > Apps or by long pressing the notifications and selecting block.
    //   - Detection works on Android 4.4+, requires Android Support v4 Library 24.0.0+
    static boolean areNotificationsEnabled(Context  context) {
-      try {
-         return NotificationManagerCompat.from(OneSignal.appContext).areNotificationsEnabled();
-      } catch (Throwable t) {}
+      // 读五次，实际读取一次，减少方法调用次数
+      if (enableFlag) {
+         enableFlag = false;
+         try {
+            noticificationState = NotificationManagerCompat.from(OneSignal.appContext).areNotificationsEnabled();
+         } catch (Throwable t) {
+         }
+      }
 
-      return true;
+      areNoticificationsEnableCount++;
+      if (areNoticificationsEnableCount > 5){
+         enableFlag = true;
+         areNoticificationsEnableCount = 0;
+      }
+      return noticificationState;
+
    }
 
    static boolean isRunningOnMainThread() {
